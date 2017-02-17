7cbf2a58093ae4187fa3f97b723bf72bf966ba03
Dominik J Pruss <dom.j.pruss@gmail.com>
2017-02-17 10:21:41 -0700

    updated motion dect

--- motionDetection.py
+++ motionDetection.py
@@ -7,7 +7,7 @@ cv2.namedWindow("Img1")
 cv2.namedWindow("GreyScale")
 cv2.namedWindow("Blobby")
 _,f=cap.read()
-avg1 = np.float32(f)
+avg1 = np.float32(f);
 
 while True:
     _,f=cap.read()
@@ -16,11 +16,18 @@ while True:
     cv2.imshow("Video",img)
     
     grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cv2.accumulateWeighted(f,avg1,0.1)
+    cv2.accumulateWeighted(blur,avg1,0.25)
+    
     res1 = cv2.convertScaleAbs(avg1)
-    cv2.imshow("Img1",f)
+
+    diff = cv2.absdiff( blur,res1)
+    diff = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    
+    
+    cv2.imshow("Img1",blur)
     cv2.imshow("avg1",res1)
     cv2.imshow("GreyScale",grey)
+    cv2.imshow("Blobbly", diff)
     k=cv2.waitKey(1)
     if k==27:
         break
