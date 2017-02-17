95e9b54f1104797d8d9aca4ffb9e7ba50d863d6d
Dominik J Pruss <dom.j.pruss@gmail.com>
2017-02-17 11:03:01 -0700

    updated changes

--- motionDetection.py
+++ motionDetection.py
@@ -3,8 +3,8 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("Video")
-cv2.namedWindow("Img1")
-cv2.namedWindow("GreyScale")
+#cv2.namedWindow("Img1")
+#cv2.namedWindow("GreyScale")
 cv2.namedWindow("Blobby")
 _,f=cap.read()
 avg1 = np.float32(f);
@@ -22,12 +22,20 @@ while True:
 
     diff = cv2.absdiff( blur,res1)
     diff = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    cv2.threshold(diff,200,255,0)
+
+    im2, contours, hierarchy = cv2.findContours(diff,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
     
+
+    cnt = contours[4]
+    cv2.drawContours(f, [cnt], 0, (0,255,0), 4)
     
-    cv2.imshow("Img1",blur)
-    cv2.imshow("avg1",res1)
-    cv2.imshow("GreyScale",grey)
-    cv2.imshow("Blobbly", diff)
+    cv2.imshow("Video",f)
+   # cv2.imshow("Img1",blur)
+    #cv2.imshow("avg1",res1)
+    #cv2.imshow("GreyScale",grey)
+    cv2.imshow("Blobby", diff)
     k=cv2.waitKey(1)
     if k==27:
         break
