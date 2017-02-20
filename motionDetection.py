1ce03b1413edf56492a65bc4b13bde699b870e3b
Dominik J Pruss <dom.j.pruss@gmail.com>
2017-02-20 13:24:26 -0700

    finished motion detection

--- motionDetection.py
+++ motionDetection.py
@@ -16,23 +16,29 @@ while True:
     cv2.imshow("Video",img)
     
     grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cv2.accumulateWeighted(blur,avg1,0.25)
+    cv2.accumulateWeighted(blur,avg1,0.5)
     
     res1 = cv2.convertScaleAbs(avg1)
 
     diff = cv2.absdiff( blur,res1)
     diff = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
-    cv2.threshold(diff,200,255,0)
-
-    im2, contours, hierarchy = cv2.findContours(diff,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
     
+    ret,diff = cv2.threshold(diff,15,255,0)
+    diff = cv2.dilate(diff, None, iterations=2)
+    im2, contours, hierarchy = cv2.findContours(diff,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    cnt = contours[4]
-    cv2.drawContours(f, [cnt], 0, (0,255,0), 4)
+    
+    
+    
+    for c in contours:
+        if cv2.contourArea(c) < 1500:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(f, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
     cv2.imshow("Video",f)
-   # cv2.imshow("Img1",blur)
+    #cv2.imshow("Img1",blur)
     #cv2.imshow("avg1",res1)
     #cv2.imshow("GreyScale",grey)
     cv2.imshow("Blobby", diff)
